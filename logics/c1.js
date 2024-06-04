8f4366ecb8ea1be5cf8024a90718d4b8ded78114
rizkih1109 <rizkihidayatulloh1305@gmail.com>
2024-06-04 21:54:47 +0700

    Challenge logics

---
+++ logics/c1.js
@@ -0,0 +1,12 @@
+function sum() {
+    let total = 0
+    for (let i = 0; i < arguments.length; i++) {
+        total += arguments[i]
+    }
+    console.log(total)
+}
+
+sum(1,2,7);
+sum(1,4);
+sum(11);
+sum(10,3,6,7,9)
