f9b5c6a9b08b71d278bd9f4fb7e9240609abd82b
rizkih1109 <rizkihidayatulloh1305@gmail.com>
2024-06-04 23:36:09 +0700

    Challenge 1 done

--- logics/c1.js
+++ logics/c1.js
@@ -1,7 +1,24 @@
+// Cara Pertama
+
 function sum() {
-    let total = 0
+    let total = 0;
     for (let i = 0; i < arguments.length; i++) {
         total += arguments[i]
+    }
+    console.log(total);
+}
+
+sum(1,2,7);
+sum(1,4);
+sum(11);
+sum(10,3,6,7,9);
+
+
+// Cara Kedua
+function sum(...goods) {
+    let total = 0;
+    for (let good of goods) {
+        total += good
     }
     console.log(total)
 }
@@ -10,3 +27,14 @@ sum(1,2,7);
 sum(1,4);
 sum(11);
 sum(10,3,6,7,9)
+
+
+// Cara Ketiga
+function sum() {
+    console.log(Object.values(arguments).reduce((total, num) => { return total + num }))
+}
+
+sum(1, 2, 7);
+sum(1, 4);
+sum(11);
+sum(10, 3, 6, 7, 9)
