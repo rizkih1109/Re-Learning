d068838ce8aa54524139110e5a10dfa65aa185d4
rizkih1109 <rizkihidayatulloh1305@gmail.com>
2024-06-10 08:44:10 +0700

    challenge 4 cek prima done

--- logics/c4.js
+++ logics/c4.js
@@ -1,3 +1,5 @@
+// Cara pertama
+
 function indexPrime(n) {
     const hasil = [];
     let khusus = n * 20;
@@ -18,4 +20,24 @@ function indexPrime(n) {
 
 }
 
-console.log(indexPrime(4));
+console.log(indexPrime(4))
+console.log(indexPrime(500))
+console.log(indexPrime(37786))
+
+// Cara kedua
+
+function indexPrime(param) {
+    let count = 0
+    for (let i =2; ; i++){
+        let cekParam = true
+        for (let j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j == 0) cekParam = false
+        }
+        if (cekParam == true) count++
+        if (count == param) return i
+    }
+}
+
+console.log(indexPrime(4))
+console.log(indexPrime(500))
+console.log(indexPrime(37786))
